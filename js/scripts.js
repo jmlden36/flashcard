1a3719441deb0441a3eb2a3638eca6982b1657eb
Matt Luker <jamesmattluker@gmail.com>
2021-10-21 08:09:56 -0600

    add css, js, and HTML

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  $(".def1").click(function() {
+    $(".def1-showing").toggle();
+    $(".def1-hidden").toggle();
+
+  });
+
+  $(".def2").click(function() {
+    $(".def2-showing").toggle();
+    $(".def2-hidden").toggle();
+
+  });
+
+  $(".def3").click(function() {
+    $(".def3-showing").toggle();
+    $(".def3-hidden").toggle();
+
+  });
+
+  $(".def4").click(function() {
+    $(".def4-showing").toggle();
+    $(".def4-hidden").toggle();
+
+  });
+
+  $(".def5").click(function() {
+    $(".def5-showing").toggle();
+    $(".def5-hidden").toggle();
+
+  });
+
+  $(".def6").click(function() {
+    $(".def6-showing").toggle();
+    $(".def6-hidden").toggle();
+
+  });
+
+
+});
